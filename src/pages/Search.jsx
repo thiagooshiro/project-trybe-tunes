f50a8b4b6e3b1f8d0656e5a44e08619fa02149fe
Thiago Oshiro <oshirothiago@gmail.com>
2021-09-16 00:15:19 -0300

    correcoes do lint

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,38 +4,38 @@ import Header from '../components/Header';
 const MIN_LENGTH = 2;
 
 class Search extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state ={
+    this.state = {
       artist: '',
-    }
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange({target}){
-    const {name , value} = target;
+  handleChange({ target }) {
+    const { name, value } = target;
     this.setState({
-      [name]:value,
-    })
+      [name]: value,
+    });
   }
 
-
   render() {
-    const { artist } = this.state
+    const { artist } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
           <form>
-            <input 
+            <input
               name="artist"
-              data-testid="search-artist-input" 
+              data-testid="search-artist-input"
               onChange={ this.handleChange }
               type="text"
               value={ artist }
             />
             <button
               data-testid="search-artist-button"
+              type="button"
               disabled={ artist.length < MIN_LENGTH }
             >
               Pesquisar
